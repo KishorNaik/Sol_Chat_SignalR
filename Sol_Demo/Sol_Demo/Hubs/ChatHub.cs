bd5cdad26b8c0257948cba69718a55cd6e3664fa
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-19 14:30:36 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await base.Clients.All.SendAsync("SendMessageToClientJsMethod", user, message);
+        }
+    }
+}
